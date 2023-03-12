cac299db58cbd6c9213cdd52b149478f1917cc68
amant2904 <tiwariaman2904@gmail.com>
2023-03-12 19:19:29 +0530

    project initialized and header created

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar';
+import HeaderImage from './HeaderImage';
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <HeaderImage />
+        </React.Fragment>
+    )
+}
+
+export default Header;
---
+++ src/Components/Layout/HeaderImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./HeaderImage.css";
+
+function HeaderImage() {
+    return (
+        <div className='header_img'>lkjlk</div>
+    )
+}
+
+export default HeaderImage;
