81e8085df967095b0e1126232888d316c2c1808a
amant2904 <tiwariaman2904@gmail.com>
2023-03-13 14:54:33 +0530

    cart open and close working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart"
 
 function App() {
+
+  const [showCart, setShowCart] = useState(false);
+
+  const cartBtn_handler = () => {
+    setShowCart(true);
+  }
+
+  const cartClose_handler = () => {
+    setShowCart(false);
+  }
+
   return (
     <React.Fragment>
-      <Header />
+      <Header cartBtn_handler={cartBtn_handler} />
       <Meals />
+      {showCart && <Cart cartClose={cartClose_handler} totalPrice="45.99" />}
     </React.Fragment>
   );
 }
