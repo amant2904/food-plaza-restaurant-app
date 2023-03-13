caa2d274da014f18292d8ca8e46e72948fb5717e
amant2904 <tiwariaman2904@gmail.com>
2023-03-13 09:48:53 +0530

    summary added

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Header.css";
 import headerImage from "../../assets/header_img.jpg";
 import CartButton from './CartButton';
+import Summary from './Summary';
 
 const Header = () => {
     return (
@@ -12,6 +13,7 @@ const Header = () => {
             </nav>
             <div>
                 <img src={headerImage} alt="Delicious Dishes" />
+                <Summary />
             </div>
         </header>
     )
