9e2dc1e2f25e88a91c08a2e666fe8b0e535fc4b2
amant2904 <tiwariaman2904@gmail.com>
2023-03-13 14:22:50 +0530

    cart box improved

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,11 +5,27 @@ import classes from "./Cart.module.css";
 import Button from "../UI/Button"
 
 const Cart = () => {
+    const cartItems = [{
+        id: "itm1",
+        name: "Sushi",
+        price: "22.99",
+        amount: "2"
+    }, {
+        id: "itm2",
+        name: "Schnitzel",
+        price: "16.50",
+        amount: "2"
+    }]
+
     return (
         <Card className={classes.backdrop}>
             <div className={classes.cartBox}>
                 <div className={classes.items}>
-                    <p>No Items Added Yet</p>
+                    <ul className={classes.cartItemList}>
+                        {cartItems.map((items) => {
+                            return <li key={items.id} className={classes.cartItem}>{items.name}</li>
+                        })}
+                    </ul>
                 </div>
                 <div className={classes.total}>
                     <h4>Total Amount</h4>
